a0a04267fa1e2b44d7ae1049d62189a1c02cc0a2
lzxsupre <1322580533@qq.com>
2020-06-27 22:04:48 +0800

    feat(*):爽

--- model/rfid.go
+++ model/rfid.go
@@ -6,8 +6,8 @@ import "github.com/mivinci/abc/time"
 type RFIDRec struct {
 	ID    int       `json:"id"`
 	UID   string    `json:"uid" form:"uid" validate:"required"`
-	Rfid  string    `json:"rfid"`
-	Type  int8      `json:"type"`
+	Rfid  string    `json:"rfid" form:"rfid"`
+	Type  int8      `json:"type" form:"type"`
 	Ctime time.Time `json:"ctime" gorm:"-"`
 }
 
